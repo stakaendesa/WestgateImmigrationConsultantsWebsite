355a126c73ce95d935a9a08944036db555348883
reccesoft <87607185+reccesoft@users.noreply.github.com>
2023-05-01 17:22:35 +0200

    Add project files.

---
+++ js/ktloader.js
@@ -0,0 +1,22 @@
+document.onreadystatechange = function () {
+    //if (document.readyState !== "complete") {
+    //    document.querySelector(
+    //        "body").style.visibility = "hidden";
+    //    document.querySelector(
+    //        "#loader").style.visibility = "visible";
+    //} else {
+    //    document.querySelector(
+    //        "#loader").style.display = "none";
+
+    //    document.querySelector(
+    //        "body").style.visibility = "visible";
+
+    //    try {
+    //        document.querySelector(
+    //            "#kt-loaderoverlay").style.display = "none";
+    //    } catch (e) {
+
+    //    }
+
+    //}
+};
